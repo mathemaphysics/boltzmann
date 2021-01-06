f9fa5b1c19f356d3f3daa00ca2135e997e87544f
Ryan P. Daly <mathemaphysics@gmail.com>
2021-01-06 07:56:50 +0000

    Code contrastive divergence training Fixes #3 Added unit test for functionality of OpenCV tools for display

---
+++ tests/test_opencv_display_state.cpp
@@ -0,0 +1,50 @@
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/plot.hpp>
+#include <boost/filesystem.hpp>
+
+#include "boltzmann/boltzmann_config.hpp"
+
+/**
+ * @brief Tests display of a basic layer state via OpenCV
+ * @test
+ *  - Take the top layer of an RBM
+ *  - Plots it as an image to the screen
+ */
+
+#define CV_IMAGE_TYPESIZE CV_8U
+typedef cv::uint8_t image_t;
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        cv::Mat image;
+        image.create(100, 100, CV_IMAGE_TYPESIZE);
+        image.forEach<image_t>(
+            [](image_t &pnt, const int *pos) -> void
+            {
+                pnt = (image_t) (rand() % 255);
+            }
+        );
+        cv::imwrite("test_opencv_display_state_output01.png", image, {cv::IMWRITE_PNG_COMPRESSION, 9});
+        boost::filesystem::path outPath("test_opencv_display_state_output01.png");
+        if (boost::filesystem::exists(outPath))
+        {
+            std::cout << "Test succeeded" << std::endl;
+            return 0;
+        }
+        else
+        {
+            std::cout << "Test failed" << std::endl;
+            return 1;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 2; // Some other unknown exception
+    }
+    
+    return 0;
+}
