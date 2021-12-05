0acbf0bec103374ec3b2e30fb9a4e2a1b71406bb
Ryan P. Daly <mathemaphysics@gmail.com>
2021-12-05 09:59:08 +0000

    Convert unit tests to GoogleTest Fixes #6

--- tests/test_activate_layer_back.cpp
+++ tests/test_activate_layer_back.cpp
@@ -14,58 +14,68 @@ using namespace boltzmann;
  *    the same output; returns 0 if close enough,
  *    1 if not
  */
-int main(int argc, char **argv)
+
+class NetworkTest: public ::testing::Test
 {
-    try
+protected:
+    void SetUp() override
+	{
+        net = new Network(3, {8, 8, 4});
+        net->setLayerState(2, {1, 1, 1, 1});
+	}
+
+    void TearDown() override
+	{
+        delete net;
+	}
+
+    const int numSamples = 10000;
+    boltzmann::Network* net;
+};
+
+TEST_F(NetworkTest, LayerActivationBack)
+{
+    std::vector<boltzFloat_t> nodeMeanStates(net->layers[1].size());
+    std::vector<boltzFloat_t> layerMeanStates(net->layers[1].size());
+    for (int n = 0; n < numSamples; n++)
     {
-        Network net(3, {8, 8, 4});
-        const int numSamples = 10000; // Get expectation values using numSamples samples
-
-        vector<boltzFloat_t> nodeMeanStates(net.layers[1].size());
-        vector<boltzFloat_t> layerMeanStates(net.layers[1].size());
-        net.setLayerState(2, {1, 1, 1, 1});
-        for (int n = 0; n < numSamples; n++)
+        net->updateLayerStateBack(1); // Already has the temperature
+        for (int i = 0; i < net->layers[1].size(); i++)
+            layerMeanStates[i] += net->layers[1][i]->state;
+        for (int i = 0; i < net->layers[1].size(); i++)
         {
-            net.updateLayerStateBack(1); // Already has the temperature
-            for (int i = 0; i < net.layers[1].size(); i++)
-                layerMeanStates[i] += net.layers[1][i]->state;
-            for (int i = 0; i < net.layers[1].size(); i++)
-            {
-                net.layers[1][i]->updateStateBack(1.0); // Give temperature in the call
-                nodeMeanStates[i] += net.layers[1][i]->state;
-            }
-        }
-        for (int i = 0; i < net.layers[1].size(); i++)
-        {
-            layerMeanStates[i] /= (boltzFloat_t)numSamples;
-            nodeMeanStates[i] /= (boltzFloat_t)numSamples;
-        }
-
-        // Output the results in case anyone is watching
-        cout << "Node results:  ";
-        for (auto node : nodeMeanStates)
-            cout << setw(7) << setprecision(3) << node;
-        cout << endl;
-        cout << "Layer results: ";
-        for (auto node : layerMeanStates)
-            cout << setw(7) << setprecision(3) << node;
-        cout << endl;
-
-        // Check that they're close enough (won't be exact)
-        for (auto tup : boost::combine(nodeMeanStates, layerMeanStates))
-        {
-            boltzFloat_t a, b;
-            boost::tie(a, b) = tup;
-
-            if (fabs(b - a) > 0.04)
-                return 1;
+            net->layers[1][i]->updateStateBack(1.0); // Give temperature in the call
+            nodeMeanStates[i] += net->layers[1][i]->state;
         }
     }
-    catch(const std::exception& e)
+    for (int i = 0; i < net->layers[1].size(); i++)
     {
-        std::cerr << e.what() << '\n';
-        return 2;
+        layerMeanStates[i] /= (boltzFloat_t)numSamples;
+        nodeMeanStates[i] /= (boltzFloat_t)numSamples;
     }
-    
-    return 0;
+
+    // Output the results in case anyone is watching
+    cout << "Node results:  ";
+    for (auto node : nodeMeanStates)
+        cout << setw(7) << setprecision(3) << node;
+    cout << endl;
+    cout << "Layer results: ";
+    for (auto node : layerMeanStates)
+        cout << setw(7) << setprecision(3) << node;
+    cout << endl;
+
+    // Check that they're close enough (won't be exact)
+    for (auto tup : boost::combine(nodeMeanStates, layerMeanStates))
+    {
+        boltzFloat_t a, b;
+        boost::tie(a, b) = tup;
+
+        EXPECT_LE(fabs(b - a), 0.04);
+    }
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
